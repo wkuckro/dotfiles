aa0bac7869c0b4488310ee725bfcdeec8104d1d5
Will Kuckro <wkuckro@luc.id>
2016-09-29 20:34:05 -0500

    adding brew files

---
+++ brew.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Update Homebrew and any existing formulae
+brew update
+brew upgrade --all
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install wget
+brew install wget --with-iri
+
+# Install more recent versions of some macOS tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+brew install homebrew/php/php56 --with-gmp
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install aircrack-ng
+brew install bfg
+brew install binutils
+brew install binwalk
+brew install cifer
+brew install dex2jar
+brew install dns2tcp
+brew install fcrackzip
+brew install foremost
+brew install hashpump
+brew install hydra
+brew install john
+brew install knock
+brew install netpbm
+brew install nmap
+brew install pngcheck
+brew install socat
+brew install sqlmap
+brew install tcpflow
+brew install tcpreplay
+brew install tcptrace
+brew install ucspi-tcp
+brew install xpdf
+brew install xz
+
+# Install other useful binaries.
+brew install ack
+brew install cmake
+brew install dark-mode
+brew install git
+brew install git-lfs
+brew install mysql
+brew install nvm
+brew install openssl
+brew install pv
+brew install python3
+brew install ruby
+brew install speedtest_cli
+brew install ssh-copy-id
+brew install testssl
+brew install tree
+brew install vbindiff
+brew install webkit2png
+
+# Remove outdated versions from the cellar.
+brew cleanup
