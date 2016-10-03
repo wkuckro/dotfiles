43449c24ef188c0c1bce0a680b8e3034b185987a
Will Kuckro <wkuckro@luc.id>
2016-10-03 12:33:50 -0500

    adding gitignore global file and updting symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile vim"    # list of files/folders to symlink in homedir
+files="bashrc bash_profile vim gitignore_global"    # list of files/folders to symlink in homedir
 
 ##########
 
