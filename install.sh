0b037e22803d1690b416c884a468548be6024bf2
Will Kuckro <wkuckro@luc.id>
2016-10-03 22:08:31 -0500

    updaiting instal script, not yet done

--- install.sh
+++ install.sh
@@ -1,10 +1,83 @@
 #!/bin/sh
 
-#Going to install some critical components needed to run the rest of the scripts.
+#############################
+# First things first
+#############################
+
+#Install some critical components needed to run the rest of the scripts.
+# Ask for the administrator password upfront
+echo "Enter you password now once instead of many times later"
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+#############################
+#Get Gitub information set up
+#############################
+grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
+if [[ $? = 0 ]]; then
+    read -r -p "What is your github.com username? " githubuser
+
+  fullname=`osascript -e "long user name of (system info)"`
+
+  if [[ -n "$fullname" ]];then
+    lastname=$(echo $fullname | awk '{print $2}');
+    firstname=$(echo $fullname | awk '{print $1}');
+  fi
+
+  if [[ -z $lastname ]]; then
+    lastname=`dscl . -read /Users/$(whoami) | grep LastName | sed "s/LastName: //"`
+  fi
+  if [[ -z $firstname ]]; then
+    firstname=`dscl . -read /Users/$(whoami) | grep FirstName | sed "s/FirstName: //"`
+  fi
+  email=`dscl . -read /Users/$(whoami)  | grep EMailAddress | sed "s/EMailAddress: //"`
+
+  if [[ ! "$firstname" ]];then
+    response='n'
+  else
+    echo -e "I see that your full name is $COL_YELLOW$firstname $lastname$COL_RESET"
+    read -r -p "Is this correct? [Y|n] " response
+  fi
+
+  if [[ $response =~ ^(no|n|N) ]];then
+    read -r -p "What is your first name? " firstname
+    read -r -p "What is your last name? " lastname
+  fi
+  fullname="$firstname $lastname"
+
+  bot "Great $fullname, "
+
+  if [[ ! $email ]];then
+    response='n'
+  else
+    echo -e "The best I can make out, your email address is $COL_YELLOW$email$COL_RESET"
+    read -r -p "Is this correct? [Y|n] " response
+  fi
+
+  if [[ $response =~ ^(no|n|N) ]];then
+    read -r -p "What is your email? " email
+    if [[ ! $email ]];then
+      error "you must provide an email to configure .gitconfig"
+      exit 1
+    fi
+  fi
 
 
-#Install Node Version Manager
+
+#############################
+#Install Node Vand components
+#############################
+
+# Install Node Version Manager
 echo -e "\n\nInstalling Node (from nvm)"
 echo "=============================="
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+
+echo "Check if the installation was a sucess"
+if (command -v nvm)  #https://github.com/creationix/nvm
+
+# Install Node after NVM complete
+nvm install node
