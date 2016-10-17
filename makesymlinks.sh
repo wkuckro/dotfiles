78c978d2b8a7ada42c7cc6f28b326c5d57b6c5da
wkuckro <wkuckro@users.noreply.github.com>
2016-10-17 10:03:38 -0500

    Update makesymlinks.sh
    adding bash_aliases

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_profile vim gitignore_global functions"    # list of files/folders to symlink in homedir
+files="bashrc bash_aliases bash_profile vim gitignore_global functions"    # list of files/folders to symlink in homedir
 
 ##########
 
